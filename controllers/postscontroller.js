6a2db2ad7b432c1eeca7efc310f6ffe363c79651
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-20 19:22:35 +0100

    add index

--- controllers/postscontroller.js
+++ controllers/postscontroller.js
@@ -1,7 +1,13 @@
 const posts = require("../data/posts.js")
 
 function index(req, res) {
-    res.send("lista dei post")
+    let filteredPosts = posts;
+    if (req.query.tag) {
+        filteredPosts = posts.filter(
+            post => post.tags.includes(req.query.tag)
+        )
+    }
+    res.json(filteredPosts)
 };
 
 function show(req, res) {
